0efee0ad840cb60d803bd1cc142a50f8696d6516
Richard Jones <richard@mechanicalcat.net>
2014-05-11 08:29:51 +1000

    modernise manage.py; record dev usage; fix up static configuration

--- pyweek/urls.py
+++ pyweek/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-     url(r'^admin/', include(admin.site.urls)),
-     (r'', include('pyweek.challenge.urls')),
-)
-
-urlpatterns += staticfiles_urlpatterns()
+    url(r'^admin/', include(admin.site.urls)),
+    (r'', include('pyweek.challenge.urls')),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
