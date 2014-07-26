69ac19d5fba5ef1c72b70f37e15dfd769fdf6714
Daniel Pope <mauve@mauveweb.co.uk>
2014-07-26 11:51:10 +0200

    Tweak a couple of config settings to make it easier to develop

--- pyweek/urls.py
+++ pyweek/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
