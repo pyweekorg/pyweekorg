ec3dccc8e55af497fb331353c566a819c4bdfa2e
Daniel Pope <mauve@mauveweb.co.uk>
2018-04-11 17:06:22 +0100

    Add temporary WSGI middleware to work around missing headers from Apache

--- pyweek/wsgi.py
+++ pyweek/wsgi.py
@@ -10,3 +10,14 @@ application = get_wsgi_application()
 from raven.contrib.django.middleware.wsgi import Sentry
 application = Sentry(application)
 
+raw_app = application
+
+
+def application(environ, start_response):
+    """Quick WSGI middleware to set host and scheme headers."""
+    environ.update(
+        HTTP_HOST='pyweek.org',
+        HTTP_X_FORWARDED_PROTO='https',
+    )
+    return raw_app(environ, start_response)
+
