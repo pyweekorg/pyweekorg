d5d5780118d05a3f3769c0784d2ddc95899b0f36
Daniel Pope <mauve@mauveweb.co.uk>
2021-09-20 22:53:02 +0100

    Fix: allow <img src=> attribute

--- pyweek/bleaching.py
+++ pyweek/bleaching.py
@@ -11,13 +11,23 @@ import bleach
 
 SAFE_TAGS = '''strong em blockquote pre b a i br img table tr th td pre p dl dd dt ul ol li span div'''.split()
 
+SAFE_ATTRIBUTES = {
+    'a': ['href', 'title'],
+    'img': ['src', 'alt'],
+}
+
 
 def html2safehtml(text: str, tags: list[str] = SAFE_TAGS) -> str:
     """Return a cleaned version of the given text.
 
     This is a wrapper around bleach to centralise the options we pass to it.
     """
-    return bleach.clean(text, tags=tags, strip=True)
+    return bleach.clean(
+        text,
+        tags=tags,
+        attributes=SAFE_ATTRIBUTES,
+        strip=True
+    )
 
 
 def html2text(text: str) -> str:
