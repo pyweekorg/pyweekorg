36e0d4bb2f0aaeb5bf3c3056b5c7af56af3d6578
Daniel Pope <mauve@mauveweb.co.uk>
2026-04-03 00:10:00 +0100

    Tune gunicorn configuration

--- deploy/runserver.sh
+++ deploy/runserver.sh
@@ -4,6 +4,7 @@ PROJDIR="/home/pyweek/www"
 PORT=8765
 PIDFILE="/home/pyweek/www/gunicorn.pid"
 LOGFILE="/home/pyweek/www/gunicorn.log"
+ACCESS_LOGFILE="/home/pyweek/www/access.log"
 VENV="/home/pyweek/www/venv"
 
 export DJANGO_SETTINGS_MODULE=prod_settings
@@ -31,7 +32,12 @@ function start () {
     cd $PROJDIR
 
     echo "Starting gunicorn"
-    $VENV/bin/gunicorn --bind localhost:$PORT -w 3 --timeout 300 --pid "$PIDFILE" --log-file "$LOGFILE" -D pyweek.wsgi:application
+    $VENV/bin/gunicorn --bind localhost:$PORT \
+	    -w 3 --threads 20 \
+	    --max-requests 600 --max-requests-jitter 100 \
+	    --pid "$PIDFILE" --log-file "$LOGFILE" \
+	    -D pyweek.wsgi:application \
+	    --access-logfile "$ACCESS_LOGFILE"
 }
 
 case $1 in
